4835ab1c6a4c8cb53d0f0a7c479db125ca440dda
ahimmelstoss <ahimmelstoss@gmail.com>
2013-11-04 16:03:19 -0500

    finished

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,2 +1,52 @@
 class TadpolesController < ApplicationController
+  before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :evolve]
+
+  def index
+    @tadpoles = Tadpole.all
+  end
+
+  def new    
+    @frog = Frog.find(params[:frog_id])
+  end
+
+  def create
+    Tadpole.create(tadpole_params)
+    redirect_to tadpoles_path
+  end
+
+  def show
+  end
+
+  def edit
+    @ponds = Pond.all
+    @frogs = Frog.all
+  end
+
+  def update
+    @tadpole.update_attributes(tadpole_params)
+    redirect_to tadpoles_path
+  end
+
+  def destroy
+    @tadpole.destroy
+    redirect_to tadpoles_path
+  end
+
+  def evolve
+    @frog = Frog.new
+    @frog.name = @tadpole.name
+    @frog.color = @tadpole.color
+    @frog.pond = @tadpole.frog.pond
+    if @frog.save && @tadpole.destroy
+      redirect_to frogs_path
+    end
+  end
+
+  private
+  def set_tadpole
+    @tadpole = Tadpole.find_by(:id => params[:id])
+  end
+  def tadpole_params
+    params.require(:tadpole).permit(:name, :color, :frog_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ PollywogRanchRailsRuby003::Application.routes.draw do
   resources :ponds
   resources :frogs
   resources :tadpoles
+  get 'tadpoles/:id/evolve' => 'tadpoles#evolve'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
